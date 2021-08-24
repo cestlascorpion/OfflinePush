f0775c69e14e92e7836661eca044f2a3ff5b9b6c
cestlascorpion <cestlascorpion@gmail.com>
2021-08-24 22:08:46 +0800

    add push

---
+++ push/server.go
@@ -0,0 +1,64 @@
+package push
+
+import (
+	"context"
+	. "github.com/cestlascorpion/offlinepush/core"
+	pb "github.com/cestlascorpion/offlinepush/proto"
+)
+
+type Server struct {
+}
+
+func NewServer(conf *PushConfig) (*Server, error) {
+	return &Server{}, nil
+}
+
+func (s *Server) PushToSingle(context.Context, *pb.PushToSingleReq) (*pb.PushToSingleResp, error) {
+	out := &pb.PushToSingleResp{}
+
+	return out, nil
+}
+
+func (s *Server) CreateTask(context.Context, *pb.CreateTaskReq) (*pb.CreateTaskResp, error) {
+	out := &pb.CreateTaskResp{}
+
+	return out, nil
+}
+
+func (s *Server) PushToList(context.Context, *pb.PushToListRep) (*pb.PushToListResp, error) {
+	out := &pb.PushToListResp{}
+
+	return out, nil
+}
+
+func (s *Server) PushToApp(context.Context, *pb.PushToAppReq) (*pb.PushToAppResp, error) {
+	out := &pb.PushToAppResp{}
+
+	return out, nil
+}
+
+func (s *Server) StopTask(context.Context, *pb.StopTaskReq) (*pb.StopTaskResp, error) {
+	out := &pb.StopTaskResp{}
+
+	return out, nil
+}
+
+func (s *Server) CheckTask(context.Context, *pb.CheckTaskReq) (*pb.CheckTaskResp, error) {
+	out := &pb.CheckTaskResp{}
+
+	return out, nil
+}
+
+func (s *Server) RemoveTask(context.Context, *pb.RemoveTaskReq) (*pb.RemoveTaskResp, error) {
+	out := &pb.RemoveTaskResp{}
+
+	return out, nil
+}
+
+func (s *Server) ViewDetail(context.Context, *pb.ViewDetailReq) (*pb.ViewDetailResp, error) {
+	out := &pb.ViewDetailResp{}
+
+	return out, nil
+}
+
+func (s *Server) Close() {}
---
+++ server/push/push.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net"
+
+	"github.com/cestlascorpion/offlinepush/core"
+	"github.com/cestlascorpion/offlinepush/proto"
+	"github.com/cestlascorpion/offlinepush/push"
+	"github.com/jinzhu/configor"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", core.AuthServerAddr)
+	if err != nil {
+		log.Fatalf("listen failed err %+v", err)
+		return
+	}
+	conf := &core.PushConfig{}
+	err = configor.Load(conf, "conf.yml")
+	if err != nil {
+		log.Fatalf("config failed err %+v", err)
+		return
+	}
+	svr, err := push.NewServer(conf)
+	if err != nil {
+		log.Fatalf("new server failed err %+v", err)
+		return
+	}
+	defer svr.Close()
+
+	s := grpc.NewServer()
+	proto.RegisterPushServer(s, svr)
+	reflection.Register(s)
+
+	err = s.Serve(lis)
+	if err != nil {
+		log.Fatalf("serve failed err %+v", err)
+		return
+	}
+}
